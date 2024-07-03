bb4a86faa8a33773602e72c2da054234d4949c8e
Obaid-ur-Rehman <obaidurrehman555@gmail.com>
2024-07-03 09:41:30 +0500

    working on highlights section and video carousel

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,7 +47,7 @@ const Hero = () => {
                 id='cta'
                 className='flex flex-col items-center opacity-0 -translate-y-20'
             >
-                <a href='#highliights' className='btn'>Buy</a>
+                <a href='#highlights' className='btn'>Buy</a>
                 <p className='font-normal text-xl'>From $199/month or $999</p>
             </div>
         </section>
